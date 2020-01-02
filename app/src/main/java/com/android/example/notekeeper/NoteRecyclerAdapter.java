55524282764ba5ad9c75f2338715c42a01fb9169
mtondolo <mtondolo@gmail.com>
2020-01-02 16:18:39 +0200

    Manage views with recycler adapter

--- app/src/main/java/com/android/example/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/android/example/notekeeper/NoteRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,7 +21,8 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.id.item_note_list, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -34,8 +36,14 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mTextCourse;
+        private final TextView mTextTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTextCourse = itemView.findViewById(R.id.text_course);
+            mTextTitle = itemView.findViewById(R.id.text_title);
         }
     }
 }
